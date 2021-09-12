e2ba09eb5ef26f139228052efa55ab3941071f6d
Kawsar Ahmed <kawsar@anysense.co.jp>
2021-09-12 10:24:01 +0600

    app notification added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue';
-import router from './router'
-import './assets/css/tailwind.css'
+import _ from 'lodash';
+import App from '@/App.vue';
+import router from '@/router'
+import '@/assets/css/tailwind.css'
+import VueNotify from 'vue-notifyjs';
 
 import localStorage from '@/services/localStorage';
+import notification from '@/services/notification';
 
 Vue.config.productionTip = false;
 
+require('@/bootstrap');
+
+Vue.use(VueNotify);
+
+Vue.prototype.$lodash = _;
+
 Vue.prototype.$localStorage = localStorage;
+Vue.prototype.$notification = notification;
+
+window.$ = window.jQuery = require('jquery');
 
 
 const app = new Vue({
